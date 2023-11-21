2c269e5d2811aa459853a01778ba6d2bff3597c8
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-11-21 23:55:04 +0900

    add: scaffold_messanger_serviceクラスを作成

---
+++ lib/utils/scaffold_messanger_service.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final navigatorKeyProvider = Provider(
+  (_) => GlobalKey<NavigatorState>(),
+);
+
+final scaffoldMessengerKeyProvider = Provider(
+  (ref) => GlobalKey<ScaffoldMessengerState>(),
+);
+
+final scaffoldMessengerServiceProvider =
+    Provider.autoDispose(ScaffoldMessengerService.new);
+
+class ScaffoldMessengerService {
+  ScaffoldMessengerService(this._ref);
+
+  final AutoDisposeProviderRef<ScaffoldMessengerService> _ref;
+
+  GlobalKey<ScaffoldMessengerState> get scaffoldKey =>
+      _ref.read(scaffoldMessengerKeyProvider);
+
+  /// スナックバーを表示する。
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSuccessSnackBar(
+    String message, {
+    bool removeCurrentSnackBar = true,
+    Duration duration = const Duration(seconds: 3),
+  }) {
+    final scaffoldMessengerState = scaffoldKey.currentState!;
+    if (removeCurrentSnackBar) {
+      scaffoldMessengerState.removeCurrentSnackBar();
+    }
+    return scaffoldMessengerState.showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: duration,
+      ),
+    );
+  }
+
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason>
+      showExceptionSnackBar(
+    String message, {
+    bool removeCurrentSnackBar = true,
+    Duration duration = const Duration(seconds: 3),
+  }) {
+    final scaffoldMessengerState = scaffoldKey.currentState!;
+    if (removeCurrentSnackBar) {
+      scaffoldMessengerState.removeCurrentSnackBar();
+    }
+    return scaffoldMessengerState.showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: duration,
+      ),
+    );
+  }
+}
