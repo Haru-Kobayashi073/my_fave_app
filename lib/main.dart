9bbe2a8d8a9a1c41f2294b402d8d0034e8e71684
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-11-22 11:12:19 +0900

    add: firebaseとdotenvのinitializeを記述

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:my_fave_app/app.dart';
 
+
 Future<void> main() async {
-  runApp(const ProviderScope(child: App()));
+  WidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load();
+  await Firebase.initializeApp();
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
