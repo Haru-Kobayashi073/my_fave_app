0ed5add3664db26df941dc74bf7a3994870e1bff
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-11-26 14:07:30 +0900

    remove: dotenv未使用のため、削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:my_fave_app/app.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    await dotenv.load();
   await Firebase.initializeApp();
   runApp(
     const ProviderScope(
