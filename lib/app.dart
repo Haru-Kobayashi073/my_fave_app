72761db8e002eb2567d4171b152706ceb34f6587
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-11-28 22:48:56 +0900

    add: MaterialAppにRouterを設定

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:my_fave_app/pages/authentication/authentication_page.dart';
 import 'package:my_fave_app/utils/utils.dart';
 import 'package:my_fave_app/widgets/widget.dart';
 
@@ -9,15 +8,18 @@ class App extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-      navigatorKey: ref.watch(navigatorKeyProvider),
+    final router = ref.watch(goRouterProvider);
+
+    return MaterialApp.router(
+      routerDelegate: router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
       scaffoldMessengerKey: ref.watch(scaffoldMessengerKeyProvider),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         scaffoldBackgroundColor: AppColor.black00,
         useMaterial3: false,
       ),
-      home: const AuthenticationPage(),
       builder: (BuildContext context, Widget? child) {
         return GestureDetector(
           onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,4 +2,5 @@ export 'constants/constants.dart';
 export 'extensions/extensions.dart';
 export 'gen/assets.gen.dart';
 export 'hooks/hooks.dart';
+export 'routes/routes.dart';
 export 'scaffold_messanger_service.dart';
