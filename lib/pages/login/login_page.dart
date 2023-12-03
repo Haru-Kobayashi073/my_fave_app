5dd54e9c36a563979df82c7d4cd82f7348edd505
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-12-03 15:08:49 +0900

    update: ログインページにValidationを追加

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:my_fave_app/utils/utils.dart';
 import 'package:my_fave_app/widgets/widget.dart';
@@ -11,57 +10,69 @@ class LoginPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final emailController = useTextEditingController();
     final passwordController = useTextEditingController();
+    final formKey = useFormStateKey();
+    final validateMode = useState<AutovalidateMode>(AutovalidateMode.disabled);
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.black00,
-        title: SvgPicture.asset(
-          Assets.icons.myFaveLogo,
-          width: 100,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'ログイン',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      appBar: const CommonAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'ログイン',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 24),
-            CommonTextField(
-              labelText: 'メールアドレス',
-              controller: emailController,
-            ),
-            const SizedBox(height: 8),
-            CommonTextField(
-              labelText: 'パスワード',
-              controller: passwordController,
-            ),
-            CommonButton(
-              onPressed: () {},
-              text: 'ログイン',
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                'パスワードを忘れた方はこちら',
-                style: TextStyle(
-                  color: AppColor.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+                const SizedBox(height: 24),
+                CommonTextField(
+                  labelText: 'メールアドレス',
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: Validator.email,
+                  autovalidateMode: validateMode.value,
                 ),
-              ),
+                const SizedBox(height: 8),
+                CommonTextField(
+                  labelText: 'パスワード',
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  validator: Validator.password,
+                  autovalidateMode: validateMode.value,
+                ),
+                CommonButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                    } else {
+                      validateMode.value = AutovalidateMode.onUserInteraction;
+                    }
+                  },
+                  text: 'ログイン',
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'パスワードを忘れた方はこちら',
+                    style: TextStyle(
+                      color: AppColor.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 184),
+              ],
             ),
-            const SizedBox(height: 184),
-          ],
+          ),
         ),
       ),
     );
