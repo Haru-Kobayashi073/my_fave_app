32b2998280697d5c603265e076c5ec4a8f860878
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-12-13 00:15:16 +0900

    add: オンボーディング紹介ページを作成

---
+++ lib/pages/on_boarding/on_boarding_introduction_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:my_fave_app/pages/on_boarding/components/on_boarding_components.dart';
+import 'package:my_fave_app/utils/utils.dart';
+import 'package:my_fave_app/widgets/widget.dart';
+
+class OnBoardignIntroductionPage extends StatelessWidget {
+  const OnBoardignIntroductionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CommonAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 32, bottom: 8),
+                  child: Text(
+                    '初めての方には操作方法や機能について教えるよ！',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: OnBoardingSteps.values
+                      .map(
+                        (onBoardingSteps) => OnBoardingStepTile(
+                          onBoardingSteps: onBoardingSteps,
+                        ),
+                      )
+                      .toList(),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40),
+              child: CommonButton(
+                onPressed: () {},
+                text: '次へ',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
