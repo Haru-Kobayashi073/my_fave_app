dc859e25c4c7690c9cb7ac6b43b3c9f494906743
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-12-20 13:14:28 +0900

    add: UserRepositoryの実装

---
+++ lib/repositories/user/user_repository_impl.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_fave_app/models/user_data.dart';
+import 'package:my_fave_app/repositories/user/user_repository.dart';
+import 'package:my_fave_app/utils/utils.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_repository_impl.g.dart';
+
+@Riverpod(keepAlive: true)
+UserRepositoryImpl userRepositoryImpl(UserRepositoryImplRef ref) {
+  return UserRepositoryImpl(ref);
+}
+
+class UserRepositoryImpl implements UserRepository {
+  UserRepositoryImpl(ProviderRef<UserRepository> ref)
+      : _auth = ref.read(getFirebaseAuthProvider),
+        _firestore = ref.read(getFirebaseFirestoreProvider);
+
+  final FirebaseAuth _auth;
+  final FirebaseFirestore _firestore;
+
+  @override
+  User? get currentUser => _auth.currentUser;
+
+  @override
+  Future<void> createUser({
+    String? userName,
+    DateTime? birthDay,
+    String? gender,
+  }) async {
+    final uid = _auth.currentUser!.uid;
+    final createdAt = Timestamp.fromDate(DateTime.now());
+    await _firestore.collection('users').doc(uid).set(
+          UserData(
+            uid: uid,
+            userName: userName,
+            birthDay: birthDay,
+            gender: gender,
+            createdAt: createdAt,
+          ).toJson(),
+        );
+  }
+}
---
+++ lib/repositories/user/user_repository_impl.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_repository_impl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userRepositoryImplHash() =>
+    r'175e4ea38d767187b29f77fb2b5d1d654d46d110';
+
+/// See also [userRepositoryImpl].
+@ProviderFor(userRepositoryImpl)
+final userRepositoryImplProvider = Provider<UserRepositoryImpl>.internal(
+  userRepositoryImpl,
+  name: r'userRepositoryImplProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userRepositoryImplHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UserRepositoryImplRef = ProviderRef<UserRepositoryImpl>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
