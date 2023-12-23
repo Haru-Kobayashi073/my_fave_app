73386923703f3fd6013346ae334b25e842fe90c2
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-12-23 18:04:09 +0900

    add: 独自のグラデーションの作成処理をProviderに渡す

---
+++ lib/features/gradation/gradation.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gradation.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$gradationHash() => r'ce4febd933e4993e49d44e4428265ef7e92c775b';
+
+/// See also [Gradation].
+@ProviderFor(Gradation)
+final gradationProvider =
+    AutoDisposeAsyncNotifierProvider<Gradation, ImageAndGradient>.internal(
+  Gradation.new,
+  name: r'gradationProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$gradationHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Gradation = AutoDisposeAsyncNotifier<ImageAndGradient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
