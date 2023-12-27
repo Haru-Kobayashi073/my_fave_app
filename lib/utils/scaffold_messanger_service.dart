b03bae7833c35b3718efdce39ffd7f91c81cde8a
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-12-27 14:37:52 +0900

    remove: navigatorKeyProviderを移動

--- lib/utils/scaffold_messanger_service.dart
+++ lib/utils/scaffold_messanger_service.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final navigatorKeyProvider = Provider(
-  (_) => GlobalKey<NavigatorState>(),
-);
-
 final scaffoldMessengerKeyProvider = Provider(
   (ref) => GlobalKey<ScaffoldMessengerState>(),
 );
