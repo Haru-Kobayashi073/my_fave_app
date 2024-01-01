d02a5511188f0a12a076f436faf3f11ce904d3b1
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-01 22:56:38 +0900

    add: FileRepositoryの具象クラスを作成

---
+++ lib/repositories/file/file_repository_impl.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:my_fave_app/repositories/file/file_repository.dart';
+import 'package:my_fave_app/utils/utils.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'file_repository_impl.g.dart';
+
+@riverpod
+FileRepositoryImpl fileRepositoryImpl(FileRepositoryImplRef ref) {
+  return FileRepositoryImpl(ref);
+}
+
+class FileRepositoryImpl implements FileRepository {
+  FileRepositoryImpl(ProviderRef<FileRepository> ref)
+      : _auth = ref.read(getFirebaseAuthProvider),
+        _storage = ref.read(getFirebaseStorageProvider);
+
+  final FirebaseAuth _auth;
+  final FirebaseStorage _storage;
+
+  @override
+  User? get currentUser => _auth.currentUser;
+
+  @override
+  Future<File?> pickImage() async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    final imageFile = pickedImage != null ? File(pickedImage.path) : null;
+    return imageFile;
+  }
+
+  @override
+  Future<CroppedFile?> cropImage(File? imageFile) async {
+    if (imageFile != null) {
+      final croppedFile = await ImageCropper().cropImage(
+        sourcePath: imageFile.path,
+        uiSettings: [
+          AndroidUiSettings(
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false,
+          ),
+        ],
+      );
+      if (croppedFile != null) {
+        return croppedFile;
+      }
+    }
+    return null;
+  }
+
+  @override
+  Future<String?> uploadImage(CroppedFile? croppedFile) async {
+    if (croppedFile != null) {
+      final uid = _auth.currentUser!.uid;
+      final fileName = returnJpgFileName();
+      final imageFile = File(croppedFile.path);
+      final ref = _storage.ref().child('users').child(uid).child(fileName);
+      await ref.putFile(imageFile);
+      final downloadUrl = await ref.getDownloadURL();
+      return downloadUrl;
+    }
+    return null;
+  }
+}
---
+++ lib/repositories/file/file_repository_impl.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'file_repository_impl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fileRepositoryImplHash() =>
+    r'e640d42e947bc32051735ea91410e660d1f5d39e';
+
+/// See also [fileRepositoryImpl].
+@ProviderFor(fileRepositoryImpl)
+final fileRepositoryImplProvider =
+    AutoDisposeProvider<FileRepositoryImpl>.internal(
+  fileRepositoryImpl,
+  name: r'fileRepositoryImplProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fileRepositoryImplHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FileRepositoryImplRef = AutoDisposeProviderRef<FileRepositoryImpl>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
