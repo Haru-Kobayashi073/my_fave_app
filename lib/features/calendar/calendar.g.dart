ce061ae101ce59173efb21b336ca60f6fd0d5ad7
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-03 01:30:59 +0900

    add: CalendarのNotifierを作成

---
+++ lib/features/calendar/calendar.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'calendar.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$calendarHash() => r'9dc987471630f41919180c3b0ae8419388492929';
+
+/// See also [Calendar].
+@ProviderFor(Calendar)
+final calendarProvider =
+    AutoDisposeNotifierProvider<Calendar, DateTime>.internal(
+  Calendar.new,
+  name: r'calendarProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$calendarHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Calendar = AutoDisposeNotifier<DateTime>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
