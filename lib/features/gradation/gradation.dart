5be317eb26706ddf2573d2ccf6457ce61bd538aa
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-06 02:11:53 +0900

    update: グラデーション作成ロジックの返り値をGradient型に変更

--- lib/features/gradation/gradation.dart
+++ lib/features/gradation/gradation.dart
@@ -1,14 +1,13 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as image;
-import 'package:my_fave_app/models/image_and_gradient.dart';
 import 'package:my_fave_app/utils/image_theme_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'gradation.g.dart';
 
 @riverpod
-Future<ImageAndGradient> gradation(GradationRef ref, String imgUrl) async {
+Future<Gradient> gradation(GradationRef ref, String imgUrl) async {
   final bytes = await NetworkAssetBundle(Uri.parse(imgUrl)).load(imgUrl);
   final decoded = image.decodeImage(bytes.buffer.asUint8List());
   // 処理が重いので別スレッドで実行したい
@@ -19,9 +18,5 @@ Future<ImageAndGradient> gradation(GradationRef ref, String imgUrl) async {
 //     ref.watch(imageThemeServiceProvider).sourceColorsFromImage, decoded!);
   final gradient =
       ref.watch(imageThemeServiceProvider).buildRandomGradient(colors);
-  final imageAndColors = ImageAndGradient(
-    image: CachedNetworkImageProvider(imgUrl),
-    gradient: gradient,
-  );
-  return imageAndColors;
+  return gradient;
 }
