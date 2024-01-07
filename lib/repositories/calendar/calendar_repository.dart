f6df558e774cf908f768061dfb15ce81943cffea
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-08 03:02:58 +0900

    add: CalendarRepositoryのインターフェースを作成

---
+++ lib/repositories/calendar/calendar_repository.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_fave_app/models/daily_schedule.dart';
+
+abstract class CalendarRepository {
+  User? get currentUser;
+
+  Future<void> createSchedule(DailySchedule schedule);
+}
