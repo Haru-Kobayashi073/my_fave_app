9003cb84d5d5c6035c344140993c4ddc22c4a5e5
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-08 21:58:27 +0900

    add: 推し活モデルを作成

---
+++ lib/models/activity_data.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'activity_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ActivityDataImpl _$$ActivityDataImplFromJson(Map<String, dynamic> json) =>
+    _$ActivityDataImpl(
+      id: json['id'] as String,
+      imageUrl: json['imageUrl'] as String,
+      createdAt: _$JsonConverterFromJson<String, DateTime>(
+          json['createdAt'], const DateTimeConverter().fromJson),
+    );
+
+Map<String, dynamic> _$$ActivityDataImplToJson(_$ActivityDataImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'imageUrl': instance.imageUrl,
+      'createdAt': _$JsonConverterToJson<String, DateTime>(
+          instance.createdAt, const DateTimeConverter().toJson),
+    };
+
+Value? _$JsonConverterFromJson<Json, Value>(
+  Object? json,
+  Value? Function(Json json) fromJson,
+) =>
+    json == null ? null : fromJson(json as Json);
+
+Json? _$JsonConverterToJson<Json, Value>(
+  Value? value,
+  Json? Function(Value value) toJson,
+) =>
+    value == null ? null : toJson(value);
