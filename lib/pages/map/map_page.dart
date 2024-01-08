4d5344da35393736fac3135ed60ccfc848fca7ef
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-08 18:40:56 +0900

    add: マップページにマーカー作成ボタンを作成

--- lib/pages/map/map_page.dart
+++ lib/pages/map/map_page.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:my_fave_app/utils/utils.dart';
+import 'package:my_fave_app/widgets/widget.dart';
 
 class MapPage extends HookConsumerWidget {
   const MapPage({super.key});
@@ -16,14 +19,66 @@ class MapPage extends HookConsumerWidget {
     );
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('聖地巡礼'),
-      ),
-      body: Center(
-        child: GoogleMap(
-          initialCameraPosition: kGooglePlex,
-          onMapCreated: controller.complete,
+      appBar: CommonAppBar(
+        icon: IconButton(
+          onPressed: () {},
+          icon: const FaIcon(FontAwesomeIcons.circleQuestion),
         ),
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      '聖地巡礼',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.search,
+                            color: AppColor.white,
+                          ),
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          splashRadius: 20,
+                        ),
+                        const SizedBox(width: 16),
+                        SizedBox(
+                          width: 120,
+                          height: 56,
+                          child: CommonButton(
+                            onPressed: () {},
+                            text: '聖地を追加',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: kGooglePlex,
+              onMapCreated: controller.complete,
+              myLocationButtonEnabled: false,
+            ),
+          ),
+        ],
       ),
     );
   }
