25558d50d83ffe92d9a6ab4a503a26af511d6c2a
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-10 23:02:19 +0900

    add: 推し活取得処理のインターフェースを作成

--- lib/repositories/activity/activity_repository.dart
+++ lib/repositories/activity/activity_repository.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_fave_app/models/activity_data.dart';
 
@@ -5,4 +6,6 @@ abstract class ActivityRepository {
   User? get currentUser;
 
   Future<void> createActivity(ActivityData activity);
+
+  Stream<QuerySnapshot> fetchActivities();
 }
