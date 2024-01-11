d953559902f0d8f1a941246b23d4f5ef96af3c62
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-11 19:50:00 +0900

    update: 画像アップロード処理のProviderに引数を変更

--- lib/features/file/upload_image.dart
+++ lib/features/file/upload_image.dart
@@ -1,19 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:my_fave_app/repositories/file/file_repository_impl.dart';
 import 'package:my_fave_app/utils/utils.dart';
 import 'package:my_fave_app/widgets/widget.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 final uploadImageProvider =
-    Provider.autoDispose<Future<String?> Function(CroppedFile? croppedFile)>(
-  (ref) => (croppedFile) async {
+    Provider.autoDispose<Future<String?> Function(File? file)>(
+  (ref) => (file) async {
     final read = ref.read;
     final isNetworkCheck = await isNetworkConnected();
     try {
       read(overlayLoadingWidgetProvider.notifier).update((state) => true);
       final response =
-          await read(fileRepositoryImplProvider).uploadImage(croppedFile);
+          await read(fileRepositoryImplProvider).uploadImage(file);
       read(imageModificationStatusProvider.notifier).uploaded();
       return response;
     } on Exception catch (e) {
