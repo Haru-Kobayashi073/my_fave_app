9bbb371ecedd1eb784c220f9b7f91268d88c699c
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-12 15:16:28 +0900

    add: マーカー取得ロジックをNotifierに作成

---
+++ lib/features/google_map_marker/google_map_marker.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'google_map_marker.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$googleMapMarkerHash() => r'90cb5cb3b8d15c44185b844109e6f662bd24e03c';
+
+/// See also [GoogleMapMarker].
+@ProviderFor(GoogleMapMarker)
+final googleMapMarkerProvider =
+    AutoDisposeAsyncNotifierProvider<GoogleMapMarker, void>.internal(
+  GoogleMapMarker.new,
+  name: r'googleMapMarkerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$googleMapMarkerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GoogleMapMarker = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
