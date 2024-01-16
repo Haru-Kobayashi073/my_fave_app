21464f6f0adb2f4f0b394b01df25aad7de583468
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-17 01:06:28 +0900

    update: 推し取得ProviderをStreamで取得するように変更

--- lib/features/favorite/favorite.g.dart
+++ lib/features/favorite/favorite.g.dart
@@ -6,12 +6,11 @@ part of 'favorite.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$favoriteHash() => r'ce9053392091b35ac140fcf17d2053b553defdb7';
+String _$favoriteHash() => r'bf44b36e9f0bc8fd9a272bb982045ca159ef121b';
 
 /// See also [Favorite].
 @ProviderFor(Favorite)
-final favoriteProvider =
-    AsyncNotifierProvider<Favorite, List<FavoriteData>>.internal(
+final favoriteProvider = AsyncNotifierProvider<Favorite, void>.internal(
   Favorite.new,
   name: r'favoriteProvider',
   debugGetCreateSourceHash:
@@ -20,6 +19,6 @@ final favoriteProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$Favorite = AsyncNotifier<List<FavoriteData>>;
+typedef _$Favorite = AsyncNotifier<void>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
