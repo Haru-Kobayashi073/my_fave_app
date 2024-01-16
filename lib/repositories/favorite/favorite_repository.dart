29266ce964248c2a1bd27547fa61761177c21704
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-17 01:00:56 +0900

    update: 推し取得ロジックをStreamで取得するように変更

--- lib/repositories/favorite/favorite_repository.dart
+++ lib/repositories/favorite/favorite_repository.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_fave_app/models/favorite_data.dart';
 
@@ -6,7 +7,7 @@ abstract class FavoriteRepository {
 
   Future<void> createFavorite(FavoriteData favoriteData);
 
-  Future<List<FavoriteData>> fetchFavoriteList();
+  Stream<QuerySnapshot> fetchFavoriteList();
 
   Future<void> editFavorite(FavoriteData favoriteData);
 
