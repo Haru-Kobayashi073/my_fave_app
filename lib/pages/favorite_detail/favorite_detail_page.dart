13b1fc17a7f2d721e631faeb18e20d5cae4ba0bd
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-17 02:01:40 +0900

    update: 推し詳細ページで直接推しを取得するように変更

--- lib/pages/favorite_detail/favorite_detail_page.dart
+++ lib/pages/favorite_detail/favorite_detail_page.dart
@@ -1,96 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:my_fave_app/features/favorite/favorite.dart';
 import 'package:my_fave_app/features/gradation/gradation.dart';
-import 'package:my_fave_app/models/favorite_data.dart';
 import 'package:my_fave_app/pages/favorite_detail/components/favorite_detail_components.dart';
 import 'package:my_fave_app/pages/favorite_detail/components/favorite_photos.dart';
 import 'package:my_fave_app/utils/utils.dart';
 import 'package:my_fave_app/widgets/widget.dart';
 
 class FavoriteDetailPage extends HookConsumerWidget {
-  const FavoriteDetailPage({super.key, required this.favoriteData});
-  final FavoriteData favoriteData;
+  const FavoriteDetailPage({super.key, required this.favoriteIndex});
+  final int favoriteIndex;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ref.watch(gradationProvider(favoriteData.imageUrl)).when(
-          data: (gradient) => DecoratedBox(
-            decoration: BoxDecoration(gradient: gradient),
-            child: Scaffold(
-              backgroundColor: Colors.transparent,
-              body: CustomScrollView(
-                slivers: [
-                  SliverAppBar(
-                    backgroundColor: Colors.transparent,
-                    titleSpacing: 0,
-                    elevation: 0,
-                    pinned: true,
-                    automaticallyImplyLeading: false,
-                    leading: Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: IconButton(
-                        onPressed: () => context.pop(),
-                        padding: EdgeInsets.zero,
-                        constraints: const BoxConstraints(),
-                        icon: const Icon(Icons.arrow_back_ios),
-                      ),
-                    ),
-                  ),
-                  FavoriteMainDetailCard(favoriteData: favoriteData),
-                  SliverToBoxAdapter(
-                    child: Container(
-                      width: double.infinity,
-                      height: context.deviceHeight * 0.1,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.transparent,
-                            AppColor.black00,
-                          ],
-                          stops: const [
-                            0.1,
-                            1.0,
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
+    final favoriteNotifier = ref.watch(favoriteProvider.notifier);
+
+    return StreamBuilder(
+      stream: favoriteNotifier.fetch(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting &&
+            !snapshot.hasData) {
+          return const Loading();
+        }
+        final favoriteList =
+            favoriteNotifier.convertToFavoriteDataList(snapshot);
+        return ref
+            .watch(gradationProvider(favoriteList[favoriteIndex].imageUrl))
+            .when(
+              data: (gradient) => DecoratedBox(
+                decoration: BoxDecoration(gradient: gradient),
+                child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: CustomScrollView(
+                    slivers: [
+                      SliverAppBar(
+                        backgroundColor: Colors.transparent,
+                        titleSpacing: 0,
+                        elevation: 0,
+                        pinned: true,
+                        automaticallyImplyLeading: false,
+                        leading: Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: IconButton(
+                            onPressed: () => context.pop(),
+                            padding: EdgeInsets.zero,
+                            constraints: const BoxConstraints(),
+                            icon: const Icon(Icons.arrow_back_ios),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  FavoritePhotos(favoriteData: favoriteData),
-                  SliverToBoxAdapter(
-                    child: Container(
-                      width: double.infinity,
-                      height: context.deviceHeight * 0.1,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.transparent,
-                            AppColor.black00,
-                          ],
-                          stops: const [
-                            0.0,
-                            0.9,
-                          ],
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
+                      FavoriteMainDetailCard(
+                        favoriteData: favoriteList[favoriteIndex],
+                      ),
+                      SliverToBoxAdapter(
+                        child: Container(
+                          width: double.infinity,
+                          height: context.deviceHeight * 0.1,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.transparent,
+                                AppColor.black00,
+                              ],
+                              stops: const [
+                                0.1,
+                                1.0,
+                              ],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      FavoritePhotos(favoriteData: favoriteList[favoriteIndex]),
+                      SliverToBoxAdapter(
+                        child: Container(
+                          width: double.infinity,
+                          height: context.deviceHeight * 0.1,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.transparent,
+                                AppColor.black00,
+                              ],
+                              stops: const [
+                                0.0,
+                                0.9,
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
-          loading: () => const Loading(),
-          error: (e, s) {
-            return Scaffold(
-              body: Center(
-                child: Text(e.toString()),
-              ),
+              loading: () => const Loading(),
+              error: (e, s) {
+                return Scaffold(
+                  body: Center(
+                    child: Text(e.toString()),
+                  ),
+                );
+              },
             );
-          },
-        );
+      },
+    );
   }
 }
