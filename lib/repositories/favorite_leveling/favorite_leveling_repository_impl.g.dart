0d14d8507714dbf03a0da846e28518fdd6b2a211
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-24 01:16:21 +0900

    add: fetchFavoriteLevelListのj実装をRepositoryに作成

---
+++ lib/repositories/favorite_leveling/favorite_leveling_repository_impl.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'favorite_leveling_repository_impl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$favoriteLevelingRepositoryImplHash() =>
+    r'b71dac130f6f6877a49005de737b93b0900863be';
+
+/// See also [favoriteLevelingRepositoryImpl].
+@ProviderFor(favoriteLevelingRepositoryImpl)
+final favoriteLevelingRepositoryImplProvider =
+    AutoDisposeProvider<FavoriteLevelingRepositoryImpl>.internal(
+  favoriteLevelingRepositoryImpl,
+  name: r'favoriteLevelingRepositoryImplProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$favoriteLevelingRepositoryImplHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FavoriteLevelingRepositoryImplRef
+    = AutoDisposeProviderRef<FavoriteLevelingRepositoryImpl>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
