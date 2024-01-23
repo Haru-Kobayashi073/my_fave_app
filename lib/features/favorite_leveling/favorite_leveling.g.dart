0bf4f2062e847a281472b908c58d4c7f8975f1dd
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-24 01:17:08 +0900

    add: 推し度に関する関数を作成

---
+++ lib/features/favorite_leveling/favorite_leveling.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'favorite_leveling.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$favoriteLevelingHash() => r'a6f91b5e51f5ca51d01fc41902c88c6037820f4d';
+
+/// See also [FavoriteLeveling].
+@ProviderFor(FavoriteLeveling)
+final favoriteLevelingProvider =
+    AutoDisposeNotifierProvider<FavoriteLeveling, void>.internal(
+  FavoriteLeveling.new,
+  name: r'favoriteLevelingProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$favoriteLevelingHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FavoriteLeveling = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
