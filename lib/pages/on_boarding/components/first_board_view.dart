1c5a58bd5279c8373822f5d429e1409b45d91c99
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-02-01 16:41:30 +0900

    add: 1~4のOnBoarding一つひとつに詳細情報を追加

--- lib/pages/on_boarding/components/first_board_view.dart
+++ lib/pages/on_boarding/components/first_board_view.dart
@@ -13,12 +13,35 @@ class FirstBoardView extends HookConsumerWidget {
       padding: const EdgeInsets.symmetric(horizontal: 32),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Container(
-            height: 320,
-            color: AppColor.black33,
+          SizedBox(
+            width: context.deviceWidth * 0.8,
+            child: Image(
+              image: AssetImage(Assets.images.firstViewApp.path),
+            ),
           ),
-          const Text('Description Text 1'),
+          const Text(
+            '機能紹介',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            '推しのいる生活をを最大限Enjoyしよう！',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            '推しの情報を綺麗に整理して、自分だけの推しページを作ろう！ \n 写真やメモなど便利機能がいっぱい！',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           CommonButton(
             onPressed: () {
               pageController.nextPage(
--- lib/pages/on_boarding/components/third_board_view.dart
+++ lib/pages/on_boarding/components/third_board_view.dart
@@ -13,17 +13,47 @@ class ThirdBoardView extends HookConsumerWidget {
       padding: const EdgeInsets.symmetric(horizontal: 32),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Container(
-            height: 320,
-            color: AppColor.black33,
+          SizedBox(
+            width: context.deviceWidth * 0.8,
+            child: Image(
+              image: AssetImage(Assets.images.firstViewApp.path),
+            ),
           ),
-          const Text('Description Text 3'),
+          const Text(
+            '機能紹介',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            '推し度をあげよう！',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            '推し度とは、推し活、聖地巡礼を行うと上がるポイントです。\n 推しのためにMaxを目指そう！',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '※アプリのログイン回数が減少すると貯めた推し度も減少します。',
+            style: TextStyle(
+              fontSize: 14,
+              color: AppColor.grey88,
+            ),
+          ),
           CommonButton(
             onPressed: () {
               pageController.nextPage(
-                duration: const Duration(milliseconds: 400),
-                curve: Curves.easeInOut,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.ease,
               );
             },
             text: '次へ',
