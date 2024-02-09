73c158ca38c869b9b3df993a5cc3688ebe08adb7
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-02-09 18:13:46 +0900

    add: ユーザー削除関数を作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// firebase-adminの初期化
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+exports.deleteUser = functions
+    .region("asia-northeast1")
+    .firestore.document("delete_users/{docId}")
+    .onCreate(async (snap) => {
+        const deleteDocument = snap.data();
+        const uid = deleteDocument?.uid;
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+        if (uid) {
+            // Authenticationのユーザーを削除する
+            await admin.auth().deleteUser(uid);
+        } else {
+            console.error("UID not found in the document data.");
+        }
+    });
