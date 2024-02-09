1661d58d19bcb82f467c665c5fcc874785deb62c
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-02-09 22:24:36 +0900

    update: 匿名ログイン時と通常時で表示ボタンを変更

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -3,14 +3,15 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:my_fave_app/features/authentication/sign_out.dart';
 import 'package:my_fave_app/features/user/delete_user.dart';
-import 'package:my_fave_app/utils/constants/constants.dart';
-import 'package:my_fave_app/utils/routes/routes.dart';
+import 'package:my_fave_app/utils/utils.dart';
 import 'package:my_fave_app/widgets/widget.dart';
 
 class SettingsPage extends HookConsumerWidget {
   const SettingsPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.read(getFirebaseAuthProvider).currentUser!;
+
     return Scaffold(
       appBar: const CommonAppBar(
         automaticallyImplyLeading: false,
@@ -28,6 +29,32 @@ class SettingsPage extends HookConsumerWidget {
               ),
             ),
             const SizedBox(height: 24),
+            if (!user.isAnonymous)
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                minLeadingWidth: 16,
+                leading: Icon(
+                  Icons.key_rounded,
+                  color: AppColor.white,
+                ),
+                title: const Text(
+                  'アカウントの再設定',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: AppColor.white,
+                ),
+                onTap: () {
+                  context.push(
+                    const ReconfigurationMailPageRoute(
+                      isReconfigurationForCertifier: true,
+                    ).location,
+                  );
+                },
+              ),
             ListTile(
               contentPadding: EdgeInsets.zero,
               minLeadingWidth: 16,
@@ -35,9 +62,9 @@ class SettingsPage extends HookConsumerWidget {
                 Icons.person,
                 color: AppColor.white,
               ),
-              title: const Text(
-                'ログアウト',
-                style: TextStyle(
+              title: Text(
+                user.isAnonymous ? '登録' : 'ログアウト',
+                style: const TextStyle(
                   fontSize: 16,
                 ),
               ),
@@ -46,6 +73,24 @@ class SettingsPage extends HookConsumerWidget {
                 color: AppColor.white,
               ),
               onTap: () async {
+                if (user.isAnonymous) {
+                  final willNavigate = await showDialog<bool>(
+                    context: context,
+                    builder: (_) => const CancelModal(
+                      title: '未ログインの状態で作成したデータも引き継がれます\n登録画面に移動しますか？',
+                    ),
+                  );
+                  if (willNavigate != true) {
+                    return;
+                  }
+                  if (!context.mounted) {
+                    return;
+                  }
+                  await context.push(
+                    const RegisterMailPageRoute().location,
+                  );
+                  return;
+                }
                 final willLogout = await showDialog<bool>(
                   context: context,
                   builder: (_) => const CancelModal(
@@ -64,73 +109,49 @@ class SettingsPage extends HookConsumerWidget {
                 );
               },
             ),
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              minLeadingWidth: 16,
-              leading: Icon(
-                Icons.key_rounded,
-                color: AppColor.white,
-              ),
-              title: const Text(
-                'アカウントの再設定',
-                style: TextStyle(
-                  fontSize: 16,
+            if (!user.isAnonymous)
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                minLeadingWidth: 16,
+                leading: Icon(
+                  Icons.delete,
+                  color: AppColor.white,
                 ),
-              ),
-              trailing: Icon(
-                Icons.arrow_forward_ios,
-                color: AppColor.white,
-              ),
-              onTap: () {
-                context.push(
-                  const ReconfigurationMailPageRoute(
-                    isReconfigurationForCertifier: true,
-                  ).location,
-                );
-              },
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              minLeadingWidth: 16,
-              leading: Icon(
-                Icons.delete,
-                color: AppColor.white,
-              ),
-              title: const Text(
-                'アカウントの削除',
-                style: TextStyle(
-                  fontSize: 16,
+                title: const Text(
+                  'アカウントの削除',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: AppColor.white,
+                ),
+                onTap: () async {
+                  final willdelete = await showDialog<bool>(
+                    context: context,
+                    builder: (_) => const CancelModal(
+                      title: 'アカウントを削除しますか？',
+                    ),
+                  );
+                  if (willdelete != true) {
+                    return;
+                  }
+                  ref.read(
+                    delteUserProvider(
+                      () {
+                        ref.read(
+                          signOutProvider(
+                            () {
+                              context.go(AuthenticationPageRoute().location);
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
               ),
-              trailing: Icon(
-                Icons.arrow_forward_ios,
-                color: AppColor.white,
-              ),
-              onTap: () async {
-                final willdelete = await showDialog<bool>(
-                  context: context,
-                  builder: (_) => const CancelModal(
-                    title: 'アカウントを削除しますか？',
-                  ),
-                );
-                if (willdelete != true) {
-                  return;
-                }
-                ref.read(
-                  delteUserProvider(
-                    () {
-                      ref.read(
-                        signOutProvider(
-                          () {
-                            context.go(AuthenticationPageRoute().location);
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                );
-              },
-            ),
           ],
         ),
       ),
