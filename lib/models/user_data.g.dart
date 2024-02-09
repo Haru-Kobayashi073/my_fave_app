18583bbdc59a887d4070f0100add6c46d11b4a4e
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-02-09 18:57:31 +0900

    update: Userモデルから性別と生年月日を削除

--- lib/models/user_data.g.dart
+++ lib/models/user_data.g.dart
@@ -10,8 +10,6 @@ _$UserDataImpl _$$UserDataImplFromJson(Map<String, dynamic> json) =>
     _$UserDataImpl(
       uid: json['uid'] as String,
       userName: json['userName'] as String?,
-      birthDay: json['birthDay'],
-      gender: json['gender'] as String?,
       createdAt: json['createdAt'],
     );
 
@@ -19,7 +17,5 @@ Map<String, dynamic> _$$UserDataImplToJson(_$UserDataImpl instance) =>
     <String, dynamic>{
       'uid': instance.uid,
       'userName': instance.userName,
-      'birthDay': instance.birthDay,
-      'gender': instance.gender,
       'createdAt': instance.createdAt,
     };
